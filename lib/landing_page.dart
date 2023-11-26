3f70c1ad13c62ae9bb1e409a039b835d7fcc3680
xxwag <93820850+xxwag@users.noreply.github.com>
2023-11-26 21:47:30 +0100

    v.0.3

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -1,17 +1,15 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:math' as math; // Import the math library
-import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:confetti/confetti.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:zmoney/device_info.dart';
 import 'package:zmoney/ngrok.dart';
-import 'package:flutter_config/flutter_config.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({Key? key}) : super(key: key);
@@ -29,6 +27,9 @@ class LandingPageState extends State<LandingPage>
   bool _isBannerAdReady = false;
   bool _showTutorial = true; // State to manage tutorial visibility
   final bool _showPartyAnimation = true; // State for party animation
+  final TextEditingController _numberController = TextEditingController();
+
+  bool _isWaitingForResponse = false;
 
   final FlutterAppAuth appAuth = FlutterAppAuth();
 
@@ -55,6 +56,8 @@ class LandingPageState extends State<LandingPage>
   void initState() {
     super.initState();
 
+    NgrokManager.fetchNgrokData();
+
     _incrementLaunchCount();
     _confettiController =
         ConfettiController(duration: const Duration(seconds: 10));
@@ -63,7 +66,7 @@ class LandingPageState extends State<LandingPage>
     // TADY SE MUSI DODELAT TUTORIAL STEPY, KAZDEJ JE NAVAZANEJ NA KEY (key1, key2) KTERYM SE MUSI OZNACIT ELEMENT WIDGETU
     // VIz. DOLE TUTORIAL STEP CLASS
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      Future.delayed(const Duration(seconds: 1), () {
+      Future.delayed(const Duration(seconds: 2), () {
         // Adjust the duration as needed
         if (mounted) {
           setState(() {
@@ -94,7 +97,7 @@ class LandingPageState extends State<LandingPage>
     _checkTutorialCompletion();
     _initBannerAd();
   }
-
+/*
   Future<Map<String, dynamic>> requestToken() async {
     // Ensure Ngrok URL is fetched and updated
     await NgrokManager.fetchNgrokData();
@@ -141,10 +144,11 @@ class LandingPageState extends State<LandingPage>
       throw Exception('Failed to request token: ${response.body}');
     }
   }
+*/
 
-  Future<Map<String, dynamic>> requestOAuthTokens() async {
+  /*Future<Map<String, dynamic>> requestOAuthTokens() async {
     // Replace these values with your OAuth configuration
-    final clientId = 'YOUR_CLIENT_ID';
+    final clientId = 'YOUR_CLIENT_ID'; 
     final redirectUri = 'YOUR_REDIRECT_URI';
     final issuer = 'YOUR_ISSUER';
     final discoveryUrl = 'YOUR_DISCOVERY_URL';
@@ -157,7 +161,7 @@ class LandingPageState extends State<LandingPage>
       final AuthorizationTokenResponse? result =
           await appAuth.authorizeAndExchangeCode(
         AuthorizationTokenRequest(
-          clientId: clientId,
+        clientId: clientId,
           redirectUri: Uri.parse(redirectUri),
           issuer: issuer,
           discoveryUrl: discoveryUrl,
@@ -180,7 +184,7 @@ class LandingPageState extends State<LandingPage>
       // Handle exceptions
       throw Exception('OAuth error: $e');
     }
-  }
+ } */
 
   void _initBannerAd() {
     _bannerAd = BannerAd(
@@ -201,25 +205,6 @@ class LandingPageState extends State<LandingPage>
     _bannerAd.load();
   }
 
-  void startTimer() {
-    if (_timer != null) {
-      _timer!.cancel(); // Cancel any existing timer
-    }
-    setState(() {
-      _timerStarted = true;
-      _remainingTime = 600; // 10 minutes in seconds
-    });
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      if (_remainingTime > 0) {
-        setState(() {
-          _remainingTime--;
-        });
-      } else {
-        _timer!.cancel();
-      }
-    });
-  }
-
   @override
   void dispose() {
     _timer?.cancel();
@@ -235,6 +220,25 @@ class LandingPageState extends State<LandingPage>
     Future.delayed(const Duration(seconds: 2), () {
       if (mounted) {
         _confettiController.stop();
+      }
+    });
+  }
+
+  void startTimer() {
+    if (_timer != null) {
+      _timer!.cancel(); // Cancel any existing timer
+    }
+    setState(() {
+      _timerStarted = true;
+      _remainingTime = 600; // 10 minutes in seconds
+    });
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_remainingTime > 0) {
+        setState(() {
+          _remainingTime--;
+        });
+      } else {
+        _timer!.cancel();
       }
     });
   }
@@ -335,6 +339,103 @@ class LandingPageState extends State<LandingPage>
     );
   }
 
+  Future<void> submitGuess() async {
+    String guess = _numberController.text;
+    if (guess.isNotEmpty &&
+        NgrokManager.ngrokUrl.isNotEmpty &&
+        !_timerStarted) {
+      setState(() {
+        _isWaitingForResponse = true; // Start waiting stage
+      });
+
+      try {
+        var response = await http.post(
+          Uri.parse('${NgrokManager.ngrokUrl}/api/guess'),
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode({'guess': guess}),
+        );
+
+        if (response.statusCode == 200) {
+          var result = json.decode(response.body);
+          bool isCorrect = result['correct'];
+
+          // Handle the result
+          setState(() {
+            _isWaitingForResponse = false; // Stop waiting stage
+            // Show result to user (win/lose)
+            _showResultDialog(
+                isCorrect); // Implement this method to show result
+          });
+        } else {
+          // Handle error
+          setState(() {
+            _isWaitingForResponse = false; // Stop waiting stage
+          });
+        }
+      } catch (e) {
+        // Handle exception
+        setState(() {
+          _isWaitingForResponse = false; // Stop waiting stage
+        });
+      }
+    }
+  }
+
+  void _showResultDialog(bool isCorrect) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(isCorrect ? 'Congratulations!' : 'Try Again!'),
+          content: Text(isCorrect ? 'You won!' : 'You lost. Please try again.'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildNumberInput(Size screenSize) {
+    return SizedBox(
+      width: screenSize.width * 0.8,
+      child: Container(
+        width: screenSize.width * 0.8,
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        decoration: ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        child: TextField(
+          key: key1, // Assign the GlobalKey here
+          controller: _numberController,
+          keyboardType: TextInputType.number,
+          inputFormatters: [
+            FilteringTextInputFormatter.digitsOnly,
+            LengthLimitingTextInputFormatter(7),
+          ],
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Enter numbers',
+          ),
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 24,
+            fontFamily: 'Inter',
+          ),
+          onTap: _playConfettiAnimation, // Play confetti on interaction
+        ),
+      ),
+    );
+  }
+
   Widget _buildTutorialOverlay() {
     if (_tutorialStep >= tutorialSteps.length) {
       return const SizedBox.shrink();
@@ -383,71 +484,38 @@ class LandingPageState extends State<LandingPage>
     }
   }
 
-  Widget _buildNumberInput(Size screenSize) {
-    return SizedBox(
-      width: screenSize.width * 0.8,
-      child: Container(
-        width: screenSize.width * 0.8,
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        decoration: ShapeDecoration(
-          color: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-        child: TextField(
-          key: key1, // Assign the GlobalKey here
-          keyboardType: TextInputType.number,
-          inputFormatters: [
-            FilteringTextInputFormatter.digitsOnly,
-            LengthLimitingTextInputFormatter(7),
-          ],
-          decoration: const InputDecoration(
-            border: InputBorder.none,
-            hintText: 'Enter numbers',
-          ),
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            fontSize: 24,
-            fontFamily: 'Inter',
-          ),
-          onTap: _playConfettiAnimation, // Play confetti on interaction
-        ),
-      ),
-    );
-  }
-
   Widget _buildGoButton(Size screenSize) {
-    return Container(
-      key: key2, // Assign the GlobalKey here
-      child: GestureDetector(
-        onTap: () {
-          startTimer();
-          _playConfettiAnimation(); // Play confetti on interaction
-        },
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          decoration: const BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Color(0x3F000000),
-                blurRadius: 4,
-                offset: Offset(0, 4),
-                spreadRadius: 0,
-              ),
-            ],
-          ),
-          child: const Text(
-            'Go!',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 40,
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w700,
+    return GestureDetector(
+      onTap: () {
+        if (!_isWaitingForResponse && !_timerStarted) {
+          submitGuess();
+        }
+      },
+      child: Container(
+        key: key2, // Assign the GlobalKey here
+        padding: const EdgeInsets.all(20),
+        decoration: const BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Color(0x3F000000),
+              blurRadius: 4,
+              offset: Offset(0, 4),
+              spreadRadius: 0,
             ),
-          ),
+          ],
         ),
+        child: _isWaitingForResponse
+            ? const CircularProgressIndicator()
+            : const Text(
+                'Go!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
       ),
     );
   }
