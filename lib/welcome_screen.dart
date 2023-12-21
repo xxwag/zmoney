3b9a32bcea25ac1092bce8cab3d0926423d0a155
xxwag <93820850+xxwag@users.noreply.github.com>
2023-12-21 20:56:20 +0100

    v.16

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -13,39 +13,83 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
-  // Create a GoogleSignIn instance
   final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+  User? currentUser;
+  bool showSkipButton = true; // Initially show the skip button
 
-  // Method to handle Google Sign-In
   Future<void> _signInWithGoogle() async {
     try {
-      // Trigger the authentication flow
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        print("Google Sign-In was cancelled.");
+        return;
+      }
 
-      // Obtain the auth details from the request
       final GoogleSignInAuthentication? googleAuth =
-          await googleUser?.authentication;
-
-      // Create a new credential
+          await googleUser.authentication;
       final credential = GoogleAuthProvider.credential(
         accessToken: googleAuth?.accessToken,
         idToken: googleAuth?.idToken,
       );
 
-      // Once signed in, return the UserCredential
       UserCredential userCredential =
           await FirebaseAuth.instance.signInWithCredential(credential);
+      print("Signed in as: ${userCredential.user?.email}");
 
-      // After the sign-in is complete, navigate to the landing page
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const LandingPage()),
-      );
+      setState(() {
+        currentUser = userCredential.user;
+        showSkipButton = false;
+      });
     } catch (error) {
-      if (kDebugMode) {
-        print(error);
-      }
-      // Handle errors here
+      print('Error during Google Sign-In: $error');
     }
+  }
+
+  Widget _buildUserInfo() {
+    if (currentUser != null) {
+      return Column(
+        children: [
+          Text('Name: ${currentUser!.displayName ?? "Not available"}'),
+          Text('Email: ${currentUser!.email ?? "Not available"}'),
+        ],
+      );
+    }
+    return SizedBox.shrink(); // Empty widget if no user
+  }
+
+  Widget _buildSignInButton() {
+    return ElevatedButton(
+      onPressed: _signInWithGoogle,
+      child: const Text('Sign in with Google'),
+    );
+  }
+
+  Widget _buildSkipButton() {
+    return Visibility(
+      visible: showSkipButton,
+      child: TextButton(
+        onPressed: () {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => const LandingPage()),
+          );
+        },
+        child: const Text('Skip'),
+      ),
+    );
+  }
+
+  Widget _buildProceedButton() {
+    return Visibility(
+      visible: currentUser != null,
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => const LandingPage()),
+          );
+        },
+        child: const Text('Proceed'),
+      ),
+    );
   }
 
   @override
@@ -55,9 +99,13 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         title: const Text('Welcome to App Lottery'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: _signInWithGoogle,
-          child: const Text('Sign in with Google'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildUserInfo(),
+            _buildSignInButton(),
+            if (showSkipButton) _buildSkipButton(),
+          ],
         ),
       ),
     );
