abc669b5e8c1d017d9ae48684cddf1cb9d1a6c5e
xxwag <93820850+xxwag@users.noreply.github.com>
2023-12-23 19:32:09 +0100

    v.17

--- android/app/src/main/kotlin/com/gg/zmoney/MainActivity.kt
+++ android/app/src/main/kotlin/com/gg/zmoney/MainActivity.kt
@@ -3,25 +3,37 @@ package com.gg.zmoney
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.RequestConfiguration
+import com.google.android.gms.games.PlayGamesSdk
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.gg.zmoney/play_games"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         // Install the SplashScreen
         installSplashScreen()
-        
+
         super.onCreate(savedInstanceState)
 
-        // Initialize the Mobile Ads SDK.
+        // Initialize the Mobile Ads SDK
         MobileAds.initialize(this) {}
 
-        // Set test device IDs
-        val requestConfiguration = RequestConfiguration.Builder()
-            .setTestDeviceIds(listOf("635AF7DDDAF79ECCB8336279257A89F1")) // Replace with your actual device ID for testing
-            .build()
-        MobileAds.setRequestConfiguration(requestConfiguration)
+        // Initialize Play Games SDK
+        PlayGamesSdk.initialize(this)
 
-        // Rest of your onCreate method...
+        // Set up the method channel
+        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "isAuthenticated") {
+                // Implement the logic to check if the user is authenticated with Play Games
+                // This is where you interact with the Play Games Services SDK
+                // For now, let's return a dummy value
+                val isAuthenticated = false // Replace with actual authentication logic
+                result.success(isAuthenticated)
+            } else {
+                result.notImplemented()
+            }
+        }
     }
 }
